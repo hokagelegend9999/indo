748f0569ff329bb011824e1950b8bd688775a49a
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-07-19 23:33:06 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -186,17 +186,17 @@ if [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g
 echo "Setup Dependencies $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
 sudo apt update -y
 apt-get install --no-install-recommends software-properties-common
-add-apt-repository ppa:vbernat/haproxy-2.0 -y
-apt-get -y install haproxy=2.0.\*
-elif [[ $(cat /etc/os-release | grep -w ID | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/ID//g') == "debian" ]]; then
-echo "Setup Dependencies For OS Is $(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')"
-curl https://haproxy.debian.net/bernat.debian.org.gpg |
-gpg --dearmor >/usr/share/keyrings/haproxy.debian.net.gpg
-echo deb "[signed-by=/usr/share/keyrings/haproxy.debian.net.gpg]" \
-http://haproxy.debian.net buster-backports-1.8 main \
->/etc/apt/sources.list.d/haproxy.list
 sudo apt-get update
-apt-get -y install haproxy=1.8.\*
+sudo apt-get install -y build-essential libpcre3-dev libssl-dev zlib1g-dev libsystemd-dev
+
+cd /usr/local/src
+wget https://www.haproxy.org/download/2.6/src/haproxy-2.6.21.tar.gz
+tar xzf haproxy-2.6.21.tar.gz
+cd haproxy-2.6.21
+make TARGET=linux-glibc \
+     USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 USE_SYSTEMD=1
+sudo make install
+
 else
 echo -e " Your OS Is Not Supported ($(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g') )"
 exit 1
